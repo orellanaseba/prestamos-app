7160c198b42c2ca3b489d857b8ecc92c0e51653d
orellanaseba <orellanasebastian2002@gmail.com>
2025-02-15 18:10:53 -0300

    add phone number

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
             </div>
             <Link className="font-bold text-lg tracking-wide text-[#2a37d8]" href="#">Prestamón</Link>
             <button className="w-20 right-2 md:block absolute text-xs md:text-sm md:w-36 border-[1px] border-zinc-300 text-white bg-[#3648f5] hover:bg-[#2a37d8] font-semibold p-2 md:right-5 rounded-md min-h-8">
-                <a href="https://wa.me/1128172947/?text=Quisiera%20más%20información%20sobre%20los%20préstamos.%20Gracias!">
+                <a href="https://wa.me/+5491164955967/?text=Quisiera%20más%20información%20sobre%20los%20préstamos.%20Gracias!">
                 Consultar
                 </a>
                 </button>
--- app/components/WhatsappIcon.tsx
+++ app/components/WhatsappIcon.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export const WhatsappIcon = () => {
     return (
-    <a href="https://wa.me/1128172947/?text=Quisiera%20más%20información%20sobre%20los%20préstamos.%20Gracias!" target="_blank">
+    <a href="https://wa.me/+5491164955967/?text=Quisiera%20más%20información%20sobre%20los%20préstamos.%20Gracias!" target="_blank">
         <Image className="w-7 md:w-10" src="/icons/whatsapp.svg" width={48} height={48} alt="Whatsapp icon" />
     </a>
         )
